5f71a5171cf0b1a3595286bb802da74d67a23916
tycjantyc <jantych3h3@gmail.com>
2024-04-12 09:08:28 +0200

    Dataset update

--- src/data/get_dataset.py
+++ src/data/get_dataset.py
@@ -11,6 +11,16 @@ URLS = ["https://drive.google.com/file/d/1vCoe9oN44j--WCI7VxZZkA7wmbot6DH0/view?
         "https://drive.google.com/file/d/1vqpv_kB0YdCUbDtnqUXE_pqHELcEIoNV/view?usp=share_link"
         ]
 
+NEW_URLS = [#"https://drive.google.com/file/d/197woxkMd3hB0ifZ2JlELTbOOwK1n7tCY/view?usp=sharing",
+            #"https://drive.google.com/file/d/1y9caSwxj17FWzJRN8iXvBtXYb6KfIT37/view?usp=sharing",
+            #"https://drive.google.com/file/d/1a3d4cFDuxJMnU8lcgxevf74LCkdBixOD/view?usp=sharing",
+            "https://drive.google.com/file/d/1NecP3yqDQ8Y8Vewj04UGxRb-GR8-mvzx/view?usp=sharing",
+            "https://drive.google.com/file/d/1jRBgW8TJGShWMDIEnpgyBTT4Pr8s7ZHr/view?usp=sharing",
+            "https://drive.google.com/file/d/1NXKTP5y2QBogUfOPxJ3Hp9cbePgUWlFo/view?usp=sharing",
+            "https://drive.google.com/file/d/1P6rAijsZi6mgbo9D-R9vZ2uSLvphOlLL/view?usp=sharing",
+            "https://drive.google.com/file/d/18XpaC-tNFkbKO5M4mLTctHDqB4AYGCD3/view?usp=sharing",
+            "https://drive.google.com/file/d/1p2qXuvb4Vk3nLPDPQFH40DvwrMxMI5CK/view?usp=sharing"]
+
 FILE_NAME = "cred_data_file"
 RAW_DATA_PATH = r'../../data/raw'
 
@@ -48,7 +58,7 @@ def clean_zip_archives() -> None:
 
 
 if __name__ == "__main__":
-    for index, url in enumerate(URLS):
+    for index, url in enumerate(NEW_URLS):
         download_file(url, fr"{RAW_DATA_PATH}/{FILE_NAME}_{index}.zip", ".zip")
         extract_from_zip(f"{RAW_DATA_PATH}/{FILE_NAME}_{index}.zip")
 
